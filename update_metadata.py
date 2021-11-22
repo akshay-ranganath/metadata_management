4a672fe57af8863d31b5dc7d6e59c2ce85c4f1a6
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2021-11-22 15:51:18 -0800

    Corrected the regex to handle multi-level folder structure

--- update_metadata.py
+++ update_metadata.py
@@ -14,7 +14,7 @@ import argparse
 RESOURCE_TYPE='image'
 #RESOURCE_TYPE='video'
 TYPE = 'upload'
-PATTERN = re.compile(r'^(?:[a-zA-Z]{2,3})?(\d+)_([^\_]+)\_([^\_]+)')
+PATTERN = re.compile(r'^(?:.+/)(?:[a-zA-Z]{2,3})?(\d+)_([^\_]+)\_([^\_]+)')
 POSITION = {
         'a': 1,
         'b': 2,
@@ -61,6 +61,9 @@ def extract_metadata(public_id):
             result += f'|display_position={POSITION[display_position_code]}'
         else:
             logging.error(f'{public_id},{display_position_code},display not found')
+    else:
+        logging.error(f'{public_id},N/A,Did not match the regex')
+    
     return result
         
 
